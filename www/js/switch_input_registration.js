7041e29f62a75a20e40700f2de5ffbf3b18891e2
matvei <matthewk@mail.ru>
2016-09-08 19:09:35 +0500

    Матвей Калин (1 коммит)

---
+++ www/js/switch_input_registration.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function() {
+
+	$("#container_registration").css("display", "none");	
+	$("#switch_input").css("background", "#fc4e3a");
+
+	$("#switch_input").click(function() {
+		$("#container_input").fadeIn("fast");	
+		$("#container_registration").css("display", "none");
+
+		$("#switch_input").css("background", "#fc4e3a");
+		$("#switch_registration").css("background", "#33ccff");
+
+		$("#container_registration").fadeOut("fast");	
+	});	
+
+	$("#switch_registration").click(function() {
+		$("#container_registration").fadeIn("fast");	
+		$("#container_input").css("display", "none");
+
+		$("#switch_input").css("background", "#33ccff");
+		$("#switch_registration").css("background", "#fc4e3a");
+
+		$("#container_input").fadeOut("fast");	
+	});			
+
+});
