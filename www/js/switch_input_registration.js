fd2699d95691d400b88189d0b78c915df75ae17e
matvei <matthewk@mail.ru>
2016-09-09 18:31:32 +0500

    Матвей Калин (3 коммит)

--- www/js/switch_input_registration.js
+++ www/js/switch_input_registration.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 		$("#container_registration").css("display", "none");
 
 		$("#switch_input").css("background", "#fc4e3a");
-		$("#switch_registration").css("background", "#33ccff");
+		$("#switch_registration").css("background", "#274473");
 
 		$("#container_registration").fadeOut("fast");	
 	});	
@@ -18,7 +18,7 @@ $(document).ready(function() {
 		$("#container_registration").fadeIn("fast");	
 		$("#container_input").css("display", "none");
 
-		$("#switch_input").css("background", "#33ccff");
+		$("#switch_input").css("background", "#274473");
 		$("#switch_registration").css("background", "#fc4e3a");
 
 		$("#container_input").fadeOut("fast");	
