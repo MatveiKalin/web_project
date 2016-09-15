dddaebcd44cb96becf15daf1f2ff57e61b11e2f0
matvei <matthewk@mail.ru>
2016-09-16 00:52:12 +0500

    Матвей Калин (Лабораторная работа №2). Коммит 4

---
+++ www/js/connect_func.js
@@ -0,0 +1,6 @@
+//document.getElementById("registration").onclick = validate;
+document.getElementById("container_input").onsubmit = validate_input;
+document.getElementById("container_registration").onsubmit = validate_registration;
+
+
+
--- www/js/switch_input_registration.js
+++ www/js/switch_input_registration.js
@@ -6,21 +6,25 @@ $(document).ready(function() {
 
 	$("#switch_input").click(function() {
 		$("#container_input").fadeIn("fast");	
+		$("#message_about_errors_input").fadeIn("fast");
 		$("#container_registration").css("display", "none");
 
 		$("#switch_input").css("background", "#fc4e3a");
 		$("#switch_registration").css("background", "#274473");
 
+		$("#message_about_errors_registration").fadeOut("fast");
 		$("#container_registration").fadeOut("fast");	
 	});	
 
 	$("#switch_registration").click(function() {
 		$("#container_registration").fadeIn("fast");	
+		$("#message_about_errors_registration").fadeIn("fast");
 		$("#container_input").css("display", "none");
 
 		$("#switch_input").css("background", "#274473");
 		$("#switch_registration").css("background", "#fc4e3a");
 
+		$("#message_about_errors_input").fadeOut("fast");
 		$("#container_input").fadeOut("fast");	
 	});			
 
